dc73b697bbbc189cac6fc206fb6e9abd7412b780
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-11-13 16:37:00 +0900

    feat: 메인화면  column

--- client/src/components/home/js/Home.js
+++ client/src/components/home/js/Home.js
@@ -8,37 +8,43 @@ import video5 from '../../../assets/video/main6.webm';
 export default function Home() {
   return (
     <>
-      <div className="mainWrap">
-        <div className="videoWrap">
-          <div className="videoLeftWrap">
-            <video className="video2" autoPlay={true} muted loop="true">
-              <source src={video2} type="video/webm" />
-            </video>
-            <video className="video4" autoPlay={true} muted loop="true">
-              <source src={video4} type="video/webm" />
-            </video>
+      <div className="home">
+        <div className="mainWrap">
+          <div className="videoWrap">
+            <div className="videoLeftWrap">
+              <video className="video2" autoPlay={true} muted loop="true">
+                <source src={video2} type="video/webm" />
+              </video>
+              <video className="video4" autoPlay={true} muted loop="true">
+                <source src={video4} type="video/webm" />
+              </video>
+            </div>
+            <div className="videoCenterWrap">
+              <video className="video1" autoPlay={true} muted loop="true">
+                <source src={video1} type="video/webm" />
+              </video>
+            </div>
+            <div className="videoRightWrap">
+              <video className="video5" autoPlay={true} muted loop="true">
+                <source src={video5} type="video/webm" />
+              </video>
+              <video className="video3" autoPlay={true} muted loop="true">
+                <source src={video3} type="video/webm" />
+              </video>
+            </div>
           </div>
-          <div className="videoCenterWrap">
-            <video className="video1" autoPlay={true} muted loop="true">
-              <source src={video1} type="video/webm" />
-            </video>
-          </div>
-          <div className="videoRightWrap">
-            <video className="video5" autoPlay={true} muted loop="true">
-              <source src={video5} type="video/webm" />
-            </video>
-            <video className="video3" autoPlay={true} muted loop="true">
-              <source src={video3} type="video/webm" />
-            </video>
+          <div className="mainText">
+            <div className="mainTextTop">
+              <h2>LUXURY, FRESH, ORGANIC ON!</h2>
+            </div>
+            <div className="mainTextBottom">
+              <h1>GO LOVE YOURDOG</h1>
+            </div>
           </div>
         </div>
-        <div className="mainText">
-          <div className="mainTextTop">
-            <h2>LUXURY, FRESH, ORGANIC ON!</h2>
-          </div>
-          <div className="mainTextBottom">
-            <h1>GO LOVE YOURDOG</h1>
-          </div>
+        <div className="main2Wrap">
+          <h3>BEST PRODUCTS</h3>
+          <div className="bestProducts"></div>
         </div>
       </div>
     </>
