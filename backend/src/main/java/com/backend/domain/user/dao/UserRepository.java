403f0fc314ccc71134131fb3ecbbd172e24442b0
taekgil99 <pjwlover2005@naver.com>
2022-11-14 17:48:44 +0900

    Feat: UserRepository

--- backend/src/main/java/com/backend/domain/user/dao/UserRepository.java
+++ backend/src/main/java/com/backend/domain/user/dao/UserRepository.java
@@ -1,4 +1,19 @@
 package com.backend.domain.user.dao;
 
-public interface UserRepository {
+import com.backend.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // existsByEmail
+    boolean existsByEmail(String email);
+
+    // existsByUsername
+    boolean existsByUserName(String username);
+
+    // findByEmail
+    Optional<User> findByEmail(String email);
+
 }
