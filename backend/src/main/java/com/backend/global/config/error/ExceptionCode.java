fe22584c94eda7d89eef143c53fd45977a8281ba
taekgil99 <pjwlover2005@naver.com>
2022-11-14 20:35:10 +0900

    Feat: 에러 처리

--- backend/src/main/java/com/backend/global/config/error/ExceptionCode.java
+++ backend/src/main/java/com/backend/global/config/error/ExceptionCode.java
@@ -3,6 +3,11 @@ package com.backend.global.config.error;
 import lombok.Getter;
 
 public enum ExceptionCode {
+
+    INVALID_INPUT_VALUE(400, "Invalid Input Value"),
+    METHOD_NOT_ALLOWED(405, " Invalid Input Value"),
+    INTERNAL_SERVER_ERROR(500, "Server Error"),
+    HANDLE_ACCESS_DENIED(403, "Access is Denied"),
     // User
     EMAIL_DUPLICATION(409, "Email Is Duplication"),
     USER_NOT_FOUND(404, "USER NOT FOUND"),
@@ -20,9 +25,9 @@ public enum ExceptionCode {
     TOKEN_ILLEGAL_ARGUMENT(400, "Token Illegal Argument"),
 
 
-    ANSWER_NOT_FOUND(404,"ANSWER_NOT_FOUND"),
+    ANSWER_NOT_FOUND(404, "ANSWER_NOT_FOUND"),
 
-    POST_NOT_FOUND(404,"POST NOT FOUND");
+    POST_NOT_FOUND(404, "POST NOT FOUND");
     @Getter
     private int status;
     @Getter
