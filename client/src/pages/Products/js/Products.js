fdafd033b3a62849ab9065b68fb418b53bcf86e5
JY <jiyeon2954@gmail.com>
2022-11-15 18:25:36 +0900

    상세페이지-상품정보component

--- client/src/pages/Products/js/Products.js
+++ client/src/pages/Products/js/Products.js
@@ -14,12 +14,14 @@ function Products() {
   }, []);
   //console.log(data);
   return (
-    <div className="itemsContainer">
-      {data &&
-        data.map((data, i) => {
-          return <ProductItem data={data} key={i} />;
-        })}
-    </div>
+    <>
+      <div className="itemsContainer">
+        {data &&
+          data.map((data, i) => {
+            return <ProductItem data={data} key={i} />;
+          })}
+      </div>
+    </>
   );
 }
 
