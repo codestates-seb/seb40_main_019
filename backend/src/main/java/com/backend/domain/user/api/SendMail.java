0d1bde60b67790a3f4422ff7b9ee60aad519633a
taekgil99 <pjwlover2005@naver.com>
2022-11-15 15:34:08 +0900

    Refactor: 이메일 전송 DTO 추가 #19

--- backend/src/main/java/com/backend/domain/user/api/SendMail.java
+++ backend/src/main/java/com/backend/domain/user/api/SendMail.java
@@ -3,6 +3,7 @@ package com.backend.domain.user.api;
 
 import com.backend.domain.user.application.AuthService;
 import com.backend.domain.user.application.RegisterMail;
+import com.backend.domain.user.dto.EmailDto;
 import com.backend.domain.user.dto.ReissueResponseDto;
 import com.backend.domain.user.dto.SignUpRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
 
+import static com.backend.domain.user.dto.EmailDto.*;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/mail")
@@ -22,9 +25,11 @@ public class SendMail {
     private final RegisterMail registerMail;
 
     // 이메일 인증
-    @PostMapping("/confirm")
     @ResponseBody
-    String mailConfirm(@RequestParam("email") String email) throws Exception {
+    @PostMapping("/confirm")
+    String mailConfirm(@RequestBody RequestSendMail emailDto) throws Exception {
+
+        String email = emailDto.getEmail();
 
         String code = registerMail.sendSimpleMessage(email);
         System.out.println("인증코드 : " + code);
