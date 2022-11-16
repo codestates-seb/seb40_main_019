71cc7542576658ded34c4133e84d3223b18474e6
Liieonking <sji12251225@gmail.com>
2022-11-16 15:16:25 +0900

    Refactor: position sticky로 변경

--- client/src/components/header/js/Nav.js
+++ client/src/components/header/js/Nav.js
@@ -1,33 +1,10 @@
 import '../css/nav.scss';
 import textLogo from '../../../assets/img/LUXMEAL.png';
-import { useEffect, useState } from 'react';
 
 export default function Nav() {
-  const [fixNav, setFixNav] = useState({});
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { capture: true }); // 스크롤 이벤트 등록
-    return () => {
-      window.removeEventListener('scroll', handleScroll); // 스크롤 이벤트 등록 제거(성능저하방지)
-    };
-  }, []);
-
-  const handleScroll = () => {
-    if (scrollY > 120) {
-      setFixNav({
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        right: '0',
-        zIndex: '2',
-      });
-    } else {
-      setFixNav({});
-    }
-  };
-
   return (
     <>
-      <div style={fixNav} className="navbar">
+      <div className="navbar">
         <nav>
           <div className="navLeft" onScroll={scroll}>
             <button>Shop</button>
