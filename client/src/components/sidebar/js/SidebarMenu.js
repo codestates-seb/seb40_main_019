aaf038a2a8b06c1ca31b22844ae74cc309851100
Liieonking <sji12251225@gmail.com>
2022-11-16 15:17:00 +0900

    Feat: 사이드바 모바일

---
+++ client/src/components/sidebar/js/SidebarMenu.js
@@ -0,0 +1,24 @@
+import '../css/sidebarMenu.scss';
+import { Link } from 'react-router-dom';
+
+function SidebarMenu() {
+  return (
+    <>
+      <div className="sidebarWrapper">
+        <Link to="/">
+          <div className="logoBox" />
+        </Link>
+        <ul className="tittleList">
+          <li>New</li>
+          <li>건식 사료</li>
+          <li>습식 사료</li>
+          <li>자연식</li>
+          <li>동결 사료</li>
+          <li>SALE</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default SidebarMenu;
