2b8d1617525609e658ff03add15e1c72ba8a583d
shinker1002 <shinker1002@naver.com>
2022-11-16 19:27:11 +0900

    Fix: merge error

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Layout from './components/layout/js/Layout';
 import Home from './pages/home/js/Home';
 import Login from './pages/login/js/Login';
 import Signup from './pages/signup/js/Signup';
-import Products from './pages/Products/js/Products';
 import OauthKakao from './pages/oauth/OauthKakao';
 import OauthGoogle from './pages/oauth/OauthGoogle';
 import { useSelector, useDispatch } from 'react-redux';
@@ -44,7 +43,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/products" element={<Products />} />
+          {/* <Route path="/products" element={<Products />} /> */}
           <Route path="oauth/kakao" element={<OauthKakao />} />
           <Route path="oauth/google" element={<OauthGoogle />} />
           <Route path="/products" element={<ShopProductList />} />
