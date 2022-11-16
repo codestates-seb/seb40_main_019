df42c298438976d8a6acde13c58a3564c6e93967
Liieonking <sji12251225@gmail.com>
2022-11-16 16:46:12 +0900

    Feat: 판매자 대시보드 레이아웃 재정리

--- client/src/pages/seller/js/Seller.js
+++ client/src/pages/seller/js/Seller.js
@@ -1,111 +1,67 @@
-import Graph from '../../../components/seller/js/Graph';
+// import Graph from '../../../components/seller/js/Graph';
+import DashboardBox from '../../../components/seller/js/DashboardBox';
 import '../css/Seller.scss';
-import graphData from '../../../components/seller/graphData.json';
+// import graphData from '../../../components/seller/graphData.json';
 
 export default function Seller() {
   return (
     <>
       <div className="sellerSidebar">sidebar</div>
       <div className="sellerDashboard">
-        <div className="dashboardBox">
-          <h2>판매현황</h2>
-          <div className="dashboardContent">
-            <div className="dashboardDetail">
-              <h3>결제 완료</h3>
-              <div>
-                <h1>25</h1>
-                <h3>건</h3>
-              </div>
+        <div className="dashboardTop">
+          <div className="dashboardHalf">
+            <div className="dashboardBox">
+              <h2>주문현황</h2>
+              <DashboardBox title={'결제 대기'} num={'25'} unit={'건'} />
+              <DashboardBox title={'신규 주문'} num={'114'} unit={'건'} />
+              <DashboardBox title={'오늘 출발'} num={'114'} unit={'건'} />
+              <DashboardBox title={'예약 구매'} num={'0'} unit={'건'} />
             </div>
-            <div className="gapline"></div>
-            <div className="dashboardDetail">
-              <h3>상품준비중</h3>
-              <div>
-                <h1>114</h1>
-                <h3>건</h3>
-              </div>
-            </div>
-            <div className="gapline"></div>
-            <div className="dashboardDetail">
-              <h3>배송 지연</h3>
-              <div>
-                <h1 className="yellow">0</h1>
-                <h3>건</h3>
-              </div>
-            </div>
-            <div className="gapline"></div>
-            <div className="dashboardDetail">
-              <h3>배송 중</h3>
-              <div>
-                <h1>21</h1>
-                <h3>건</h3>
-              </div>
-            </div>
-            <div className="gapline"></div>
-            <div className="dashboardDetail">
-              <h3>배송 완료</h3>
-              <div>
-                <h1>162</h1>
-                <h3>건</h3>
-              </div>
+          </div>
+          <div className="dashboardHalf">
+            <div className="dashboardBox">
+              <h2>배송현황</h2>
+              <DashboardBox title={'상품 준비중'} num={'114'} unit={'건'} />
+              <DashboardBox title={'배송중'} num={'21'} unit={'건'} />
+              <DashboardBox
+                title={'배송 지연'}
+                num={'0'}
+                unit={'건'}
+                yellow={'yellow'}
+              />
+              <DashboardBox title={'배송 완료'} num={'162'} unit={'건'} />
             </div>
           </div>
         </div>
         <div className="dashboardBox">
           <h2>주문 통계</h2>
-          <div className="graph">
-            <Graph data={graphData} />
-          </div>
+          <div className="graph">{/* <Graph data={graphData} /> */}</div>
         </div>
         <div className="dashboardBottom">
-          <div className="dashboardBox">
-            <h2>총 판매 금액</h2>
-            <div className="dashboardTexts">
-              <div className="dashboardText">
-                <h3>11월 예상 판매금액</h3>
-                <div className="dashboardNum">
-                  <h4>1,250,870,000</h4>
-                  <h4 className="dashboardNumText">원</h4>
-                </div>
-              </div>
-              <div className="dashboardText">
-                <h3>연간 판매금액</h3>
-                <div className="dashboardNum">
-                  <h4>15,250,870,000</h4>
-                  <h4 className="dashboardNumText">원</h4>
-                </div>
-              </div>
+          <div className="dashboardHalf">
+            <div className="dashboardBox">
+              <h2>총 판매 금액</h2>
+              <DashboardBox
+                title={'11월 예상 판매금액'}
+                num={'1,250,870,000'}
+                unit={'원'}
+              />
+              <DashboardBox
+                title={'연간 판매금액'}
+                num={'15,250,870,000'}
+                unit={'원'}
+              />
             </div>
           </div>
-          <div className="dashboardBox">
-            <h2>등록 상품</h2>
-            <div className="dashboardTexts">
-              <div className="dashboardText">
-                <h3>진열</h3>
-                <div className="dashboardNum">
-                  <h4>125</h4>
-                  <h4 className="dashboardNumText">개</h4>
-                </div>
-              </div>
-              <div className="dashboardText">
-                <h3>미진열</h3>
-                <div className="dashboardNum">
-                  <h4>0</h4>
-                  <h4 className="dashboardNumText">개</h4>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="dashboardBox">
-            <h2>리뷰</h2>
-            <div className="dashboardTexts">
-              <div className="dashboardText">
-                <h3>11월 리뷰 갯수</h3>
-                <div className="dashboardNum">
-                  <h4>1,249</h4>
-                  <h4 className="dashboardNumText">개</h4>
-                </div>
-              </div>
+          <div className="dashboardHalf">
+            <div className="dashboardBox">
+              <h2>리뷰</h2>
+              <DashboardBox title={'11월 리뷰 갯수'} num={'1442'} unit={'개'} />
+              <DashboardBox
+                title={'평점 낮은 리뷰 갯수'}
+                num={'12'}
+                unit={'개'}
+              />
             </div>
           </div>
         </div>
