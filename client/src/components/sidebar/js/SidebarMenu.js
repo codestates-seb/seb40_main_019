019c2865b13d79f1c2790c5079a3863ab1786ca6
JY <jiyeon2954@gmail.com>
2022-11-17 00:04:05 +0900

    사이드바 리팩토링

--- client/src/components/sidebar/js/SidebarMenu.js
+++ client/src/components/sidebar/js/SidebarMenu.js
@@ -1,15 +1,15 @@
 import '../css/sidebarMenu.scss';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; //수정 삭제?
 
-function SidebarMenu() {
+export default function SidebarMenu() {
   return (
-    <>
-      <div className="sidebarWrapper">
+    <div className="sideMargin">
+      <div className="sidebarContainer">
         <Link to="/">
           <div className="logoBox" />
         </Link>
         <ul className="tittleList">
-          <li>New</li>
+          <li>NEW</li>
           <li>건식 사료</li>
           <li>습식 사료</li>
           <li>자연식</li>
@@ -17,8 +17,6 @@ function SidebarMenu() {
           <li>SALE</li>
         </ul>
       </div>
-    </>
+    </div>
   );
 }
-
-export default SidebarMenu;
