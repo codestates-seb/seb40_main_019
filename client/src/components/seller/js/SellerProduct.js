a470ffefe7cf6904f4fdac42ff74246e7b7e057b
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-11-18 14:39:36 +0900

    Feat: 상품관리 페이지 css

--- client/src/components/seller/js/SellerProduct.js
+++ client/src/components/seller/js/SellerProduct.js
@@ -1,13 +1,20 @@
 import '../css/SellerProduct.scss';
 export default function SellerProduct({ item }) {
   return (
-    <div>
-      <div className="checkbox">checkbox</div>
+    <div className="sellerproduct">
       <div className="createdAt">{item.createdAt}</div>
-      <div className="titleImg">{item.img}</div>
+      <img className="titleImg" src={item.img} alt="productImg" />
       <div className="title">{item.productName}</div>
-      <div className="price">{item.price}</div>
-      <div className="edit">수정</div>
+      <div className="price">{item.price} 원</div>
+      <div className="productBtn">
+        <button className="edit">
+          <i className="fa-solid fa-pen-to-square"></i>
+        </button>
+        <h3>/</h3>
+        <button className="delete">
+          <i className="fa-solid fa-trash-can"></i>
+        </button>
+      </div>
     </div>
   );
 }
