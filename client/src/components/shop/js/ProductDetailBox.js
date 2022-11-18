dcfa818374d018a5d70ffb047c14aa7c9d3a5e6a
JY <jiyeon2954@gmail.com>
2022-11-18 13:31:00 +0900

    productDetail info 전체 수정함

--- client/src/components/shop/js/ProductDetailBox.js
+++ client/src/components/shop/js/ProductDetailBox.js
@@ -3,6 +3,7 @@ import '../css/productDetailBox.scss';
 export default function ProductDetailBox() {
   return (
     <div className="detailContainer">
+      <div className="titleImg"></div>
       <div className="detailText">DETAIL</div>
       <div className="detailImg"></div>
     </div>
