6739a68ec00d6af87f324a258815677cce09f523
shinker1002 <shinker1002@naver.com>
2022-11-20 22:31:46 +0900

    Fix: 유저 데이터 조회, 수정, 삭제 엔드포인트 수정

--- client/src/util/api/mypageUser.js
+++ client/src/util/api/mypageUser.js
@@ -6,10 +6,10 @@ axios.defaults.headers.common['Content-Type'] = 'application/json';
 const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
 
 // 유저 정보 상세 조회
-export const getUserInfo = async (userId) => {
+export const getUserInfo = async () => {
   console.log('유저 정보 상세조회 내부');
   try {
-    const res = await axios.get(`${REACT_APP_API_URL}users/${userId}`);
+    const res = await axios.get(`${REACT_APP_API_URL}users`);
 
     console.log(res);
 
@@ -19,10 +19,10 @@ export const getUserInfo = async (userId) => {
   }
 };
 // 유저 정보 수정
-export const editUserInfo = async (userId, data) => {
+export const editUserInfo = async (data) => {
   console.log('유저 정보 수정 내부');
   try {
-    const res = await axios.patch(`${REACT_APP_API_URL}users/${userId}`, data);
+    const res = await axios.patch(`${REACT_APP_API_URL}users`, data);
 
     console.log(res);
 
@@ -32,10 +32,10 @@ export const editUserInfo = async (userId, data) => {
   }
 };
 // 유저 계정 삭제
-export const deleteUserAccount = async (userId) => {
+export const deleteUserAccount = async () => {
   console.log('유저 정보 삭제 내부');
   try {
-    const res = await axios.delete(`${REACT_APP_API_URL}users/${userId}`);
+    const res = await axios.delete(`${REACT_APP_API_URL}users`);
 
     console.log(res);
 
