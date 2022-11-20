938fb641264f65781b529a379d7ad28b89cbb4a6
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-11-20 23:20:51 +0900

    Feat: 판매관리 페이지 완

--- client/src/pages/sellerDelivering/js/SellerDelivering.js
+++ client/src/pages/sellerDelivering/js/SellerDelivering.js
@@ -1,5 +1,38 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Delivery from '../../../components/seller/js/Delivery';
 import '../css/SellerDelivering.scss';
 
 export default function SellerDelivering() {
-  return <div>SellerDelivering</div>;
+  const [items, setItems] = useState();
+  useEffect(() => {
+    axios.get('http://localhost:3001/orders/').then((res) => {
+      setItems(res.data);
+    });
+  }, []);
+  return (
+    <div className="sellerProducts">
+      <div className="productTitle">
+        <h1>배송 중</h1>
+        <div className="productBtn"></div>
+      </div>
+
+      <div className="lineBold"></div>
+      <ul className="lineTitle">
+        <li>발송 처리일</li>
+        <li>상품명</li>
+        <li>배송지 정보</li>
+        <li>수량</li>
+      </ul>
+
+      {items &&
+        items.map((item) => {
+          return (
+            <div key={item.productsId}>
+              <Delivery item={item} />
+            </div>
+          );
+        })}
+    </div>
+  );
 }
