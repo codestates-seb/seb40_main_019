294ace676970a0e1ffc7980685005403de1bea22
Liieonking <sji12251225@gmail.com>
2022-11-23 13:01:05 +0900

    Feat: 헤더 주소 연결

--- client/src/components/header/js/Header.js
+++ client/src/components/header/js/Header.js
@@ -1,11 +1,17 @@
 import '../css/header.scss';
 import logo from '../../../assets/img/logo_white.png';
+import { useLocation } from 'react-router-dom';
 
 export default function Header() {
+  const location = useLocation();
   return (
     <>
       <div>
-        <div className="header">
+        <div
+          className={
+            location.pathname === '/seller' ? 'header sellerHeader' : 'header'
+          }
+        >
           <img src={logo} alt="logo" />
         </div>
       </div>
--- client/src/components/header/js/Nav.js
+++ client/src/components/header/js/Nav.js
@@ -1,5 +1,6 @@
 import '../css/nav.scss';
 import textLogo from '../../../assets/img/LUXMEAL.png';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
@@ -7,20 +8,32 @@ export default function Nav() {
       <div className="navbar">
         <nav>
           <div className="maxNav">
-            <div className="navLeft" onScroll={scroll}>
-              <button className="margin">Shop</button>
-              <button className="margin">Point</button>
-              <button>Mypage</button>
+            <div className="navLeft">
+              <Link to="/products">
+                <button className="margin">Shop</button>
+              </Link>
+              <Link to="/seller">
+                <button className="margin">Seller</button>
+              </Link>
+              <Link to="/mypage/user">
+                <button>Mypage</button>
+              </Link>
             </div>
-            <div className="textLogo">
-              <img src={textLogo} alt="textLogo" />
-            </div>
+            <Link to="/">
+              <div className="textLogo">
+                <img src={textLogo} alt="textLogo" />
+              </div>
+            </Link>
             <div className="navRight">
-              <button>
-                <i className="fa-solid fa-cart-shopping"></i>
-                Cart
-              </button>
-              <button className="margin">Login</button>
+              <Link to="/product/order">
+                <button>
+                  <i className="fa-solid fa-cart-shopping"></i>
+                  Cart
+                </button>
+              </Link>
+              <Link to="/login">
+                <button className="margin">Login</button>
+              </Link>
             </div>
           </div>
         </nav>
