80a49ebe41a4f4bd260f535c1861b978e94501f1
JY <jiyeon2954@gmail.com>
2022-11-24 17:40:19 +0900

    수량

--- client/src/components/shop/js/ProductInfoBox.js
+++ client/src/components/shop/js/ProductInfoBox.js
@@ -1,8 +1,8 @@
 import '../css/productInfoBox.scss';
 import QuantityBtn from './QuantityBtn';
 import BuyAddBtn from './BuyAddBtn';
-import ReviewStar from '../../review/js/ReviewStar';
-
+// import ReviewStar from '../../review/js/ReviewStar';
+// 별점 지우고 Quantity 밑으로 내려서 제품사진과 간격 맞춤
 export default function ProductInfoBox({ product, count, setCount }) {
   return (
     <>
@@ -12,25 +12,26 @@ export default function ProductInfoBox({ product, count, setCount }) {
           <div className="itemHeaderContainer">
             <div className="imgStarBox">
               <img src={product.titleImg} alt="productImg" />
-              <div className="reviewStar">
+              {/* <div className="reviewStar">
                 <ReviewStar clickStar={product.average} type={'small'} />
-              </div>
+              </div> */}
             </div>
-            {/* body = new, 제품명, quantity */}
+            {/* body = new, 제품명, -quantity */}
             <div className="itemBodyContainer">
               {product.new === true && <button>NEW</button>}
-
               <div className="titleBox">{product.title}</div>
-              <div className="QuantityBox">
-                <span>Quantity</span>
+            </div>
+          </div>
+          {/* footer - +Quantity total, buybtn+Nowbtn, addCartBtn */}
+          <div className="itemFooterContainer">
+            <div className="QuantityBox">
+              <div className="totalbox">Quantity</div>
+              <div>
                 <QuantityBtn count={count} setCount={setCount} />
               </div>
             </div>
-          </div>
-          {/* footer - total, buybtn+Nowbtn, addCartBtn */}
-          <div className="itemFooterContainer">
             <div className="priceBox">
-              <div className="totalbox">total</div>
+              <div className="totalBox">total</div>
               <div>{product.price * count}원</div>
             </div>
             <div className="btnBox">
