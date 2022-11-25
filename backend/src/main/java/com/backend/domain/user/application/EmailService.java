c0ed3e0939eb5a04685ad9af7489e69699a3d6d6
taekgil99 <pjwlover2005@naver.com>
2022-11-26 03:47:16 +0900

    Refactor: rename RegisterMail -> EmailService

--- backend/src/main/java/com/backend/domain/user/application/EmailService.java
+++ backend/src/main/java/com/backend/domain/user/application/EmailService.java
@@ -16,34 +16,34 @@ import java.util.Random;
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class RegisterMail {
+public class EmailService {
 
     private final JavaMailSender emailSender;
 
-    private String ePw;
+    private String tempCode;
 
     // 메일 내용 작성
-    public MimeMessage createMessage(String to) throws MessagingException, UnsupportedEncodingException {
+    public MimeMessage createMessage(String to, String page) throws MessagingException, UnsupportedEncodingException {
 
         MimeMessage message = emailSender.createMimeMessage();
 
-        message.addRecipients(RecipientType.TO, to);// 보내는 대상
-        message.setSubject("회원가입 이메일 인증");// 제목
+        message.addRecipients(RecipientType.TO, to);
+        message.setSubject(page +" 이메일 인증");
 
         String msgg = "";
         msgg += "<div style='margin:100px;'>";
         msgg += "<h1> 안녕하세요</h1>";
         msgg += "<h1> 럭셔리 애견 사료 쇼핑몰 Luxmeal 입니다</h1>";
         msgg += "<br>";
-        msgg += "<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요<p>";
+        msgg += "<p>아래 코드를 쇼핑몰에서 입력해주세요<p>";
         msgg += "<br>";
         msgg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg += "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
+        msgg += "<h3 style='color:blue;'>" + page + " 인증 코드입니다.</h3>";
         msgg += "<div style='font-size:130%'>";
         msgg += "CODE : <strong>";
-        msgg += ePw + "</strong><div><br/> "; // 메일에 인증번호 넣기
+        msgg += tempCode + "</strong><div><br/> ";
         msgg += "</div>";
-        message.setText(msgg, "utf-8", "html");// 내용, charset 타입, subtype
+        message.setText(msgg, "utf-8", "html");
         // 보내는 사람의 이메일 주소, 보내는 사람 이름
         message.setFrom(new InternetAddress("pjwlover2005@naver.com", "Luxmeal"));// 보내는 사람
 
@@ -56,18 +56,18 @@ public class RegisterMail {
         Random rnd = new Random();
 
         for (int i = 0; i < 8; i++) { // 인증코드 8자리
-            int index = rnd.nextInt(3); // 0~2 까지 랜덤, rnd 값에 따라서 아래 switch 문이 실행됨
+            int index = rnd.nextInt(2); // 0~2 까지 랜덤, rnd 값에 따라서 아래 switch 문이 실행됨
 
             switch (index) {
                 case 0:
                     key.append((char) ((int) (rnd.nextInt(26)) + 97));
                     // a~z (ex. 1+97=98 => (char)98 = 'b')
                     break;
+//                case 1:
+//                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
+//                    // A~Z
+//                    break;
                 case 1:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
-                    // A~Z
-                    break;
-                case 2:
                     key.append((rnd.nextInt(10)));
                     // 0~9
                     break;
@@ -77,11 +77,11 @@ public class RegisterMail {
         return key.toString();
     }
 
-    public String sendSimpleMessage(String to) throws Exception {
+    public String sendSimpleMessage(String to, String page) throws Exception {
 
-        ePw = createKey();
+        tempCode = createKey();
 
-        MimeMessage message = createMessage(to);
+        MimeMessage message = createMessage(to, page);
 
         try {
             emailSender.send(message);
@@ -90,7 +90,7 @@ public class RegisterMail {
             throw new IllegalArgumentException();
         }
 
-        return ePw;
+        return tempCode;
     }
 
 }
