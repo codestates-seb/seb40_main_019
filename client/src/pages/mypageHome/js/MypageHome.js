4b300d4578ef4238c961c58ab2148166495f77bb
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-11-25 15:24:16 +0900

    Feat: 마이페이지 배송현황

--- client/src/pages/mypageHome/js/MypageHome.js
+++ client/src/pages/mypageHome/js/MypageHome.js
@@ -1,5 +1,40 @@
 import '../css/MypageHome.scss';
 
 export default function MypageHome() {
-  return <div>MypageHome</div>;
+  return (
+    <div>
+      <div className="orderSummery">
+        <ul>
+          <li>
+            <div className="orderIcon">
+              <p>상품준비중</p>
+              <i className="fa-solid fa-cart-flatbed"></i>
+            </div>
+            <h1>0</h1>
+          </li>
+          <li>
+            <div className="orderIcon">
+              <p>배송중</p>
+              <i className="fa-solid fa-truck"></i>
+            </div>
+            <h1>11</h1>
+          </li>
+          <li>
+            <div className="orderIcon">
+              <p>배송완료</p>
+              <i className="fa-solid fa-gift"></i>
+            </div>
+            <h1>0</h1>
+          </li>
+          <li>
+            <div className="orderIcon">
+              <p>취소/교환/반품</p>
+              <i className="fa-solid fa-arrow-rotate-left"></i>
+            </div>
+            <h1>0</h1>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
