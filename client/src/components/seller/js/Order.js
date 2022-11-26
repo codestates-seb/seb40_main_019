de35a0a32c3187c79b327cf13abd8a092acc37ad
Liieonking <sji12251225@gmail.com>
2022-11-26 22:43:06 +0900

    Feat: 판매관리 페이지 데스크탑 완

--- client/src/components/seller/js/Order.js
+++ client/src/components/seller/js/Order.js
@@ -1,20 +1,38 @@
 import '../css/Order.scss';
 
 export default function Order({ item }) {
+  let status = '상품준비중';
+  if (item.orderStatus === 'SHIPPING') status = '배송중';
+  else if (item.orderStatus === 'SHIPPED') status = '배송 완료';
+  else if (item.orderStatus === 'CANCLE') status = '주문 취소';
   return (
     <div className="sellerproduct order">
-      <div className="createdAt">
+      {/* <div className="createdAt">
         <p>{item.orderDate}</p>
-      </div>
-      <img className="titleImg" src={item.img} alt="productImg" />
-      <div className="title">{item.productName}</div>
-      <div className="orderInfo">
-        <span>{item.orderName}</span>
-        <span>{item.receiverAddress}</span>
-      </div>
-      <div className="orderCount">{item.orderCount} 개</div>
-      <div className="productBtn">
-        <button className="delivery">발송처리</button>
+      </div> */}
+      <div className="orderContainer">
+        <div className="productBtn">
+          {status === '상품준비중' ? (
+            <button className="delivery">발송처리</button>
+          ) : (
+            <h3>{status}</h3>
+          )}
+        </div>
+        <div className="orderInfo">
+          <span>{item.receiverName}</span>
+          <span>{item.receiverAddress}</span>
+        </div>
+        <div className="orderBox">
+          {item.orderProductDtoList.map((item) => {
+            return (
+              <div className="sellerOrderContent" key={item.productId}>
+                <img className="titleImg" src={item.imgUrl} alt="productImg" />
+                <div className="title">{item.productName}</div>
+                <div className="orderCount">{item.quantity} 개</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- client/src/pages/sellerOrder/js/SellerOrder.js
+++ client/src/pages/sellerOrder/js/SellerOrder.js
@@ -1,20 +1,20 @@
 import '../css/SellerOrder.scss';
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Order from '../../../components/seller/js/Order';
-import useFetch from '../../../util/useFetch';
+// import useFetch from '../../../util/useFetch';
 
 export default function SellerOrder() {
-  // const [items, setItems] = useState();
-  // useEffect(() => {
-  //   axios.get('http://localhost:3001/orders/').then((res) => {
-  //     setItems(res.data);
-  //   });
-  // }, []);
-  const [items] = useFetch('orders/all');
-  console.log(items);
+  const [items, setItems] = useState();
+  useEffect(() => {
+    axios.get('http://localhost:3001/ordersMypage/').then((res) => {
+      setItems(res.data);
+    });
+  }, []);
+  // const [items] = useFetch('orders/all');
+  // console.log(items);
   return (
-    <div className="sellerProducts">
+    <div className="sellerProducts orderTop">
       <div className="productTitle">
         <h1>주문 내역</h1>
         <div className="productBtn"></div>
@@ -22,11 +22,10 @@ export default function SellerOrder() {
 
       <div className="lineBold"></div>
       <ul className="lineTitle">
-        <li>결제일</li>
-        <li>상품명</li>
+        <li>발송현황</li>
         <li>주문자 정보</li>
+        <li>상품정보</li>
         <li>수량</li>
-        <li>발송 처리</li>
       </ul>
 
       {items &&
