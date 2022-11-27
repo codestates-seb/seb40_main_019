7551e0845860e51a299f00db1d8381186d70df7d
taekgil99 <pjwlover2005@naver.com>
2022-11-27 18:11:11 +0900

    Feat: 유효기간 지난 RTK 삭제하는 쿼리

--- backend/src/main/java/com/backend/domain/refreshToken/dao/RefreshTokenRepository.java
+++ backend/src/main/java/com/backend/domain/refreshToken/dao/RefreshTokenRepository.java
@@ -2,12 +2,18 @@ package com.backend.domain.refreshToken.dao;
 
 import com.backend.domain.refreshToken.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     // key 값으로 refresh token 삭제
     void deleteByKey(Long key);
 
-    // key 값으로 refresh token 삭제
-    void deleteByKey(String key);
+    // refresh token 유효기간이 지난 토큰 삭제
+    @Modifying
+    @Query("delete from RefreshToken t where t.expirationDate <= ?1")
+    void deleteAllExpiredSince(Date now);
 }
