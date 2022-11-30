4cecc2da9b13b8aec306cabf6d1c94af0658ef23
shinker1002 <shinker1002@naver.com>
2022-12-01 01:53:39 +0900

    Fix: 선택한 상품 없이 구매 차단

--- client/src/pages/shopProductOrder/js/OrderMobileButton.js
+++ client/src/pages/shopProductOrder/js/OrderMobileButton.js
@@ -13,6 +13,10 @@ export default function OrderMobileButton({ totalPrice, myPoint, setModal }) {
       window.alert('판매자는 이용할 수 없습니다.');
       return;
     }
+    if (totalPrice === 0) {
+      window.alert('선택한 상품이 없습니다.');
+      return;
+    }
     if (myPoint < totalPrice + 3000) {
       window.alert('포인트가 부족합니다');
       return;
