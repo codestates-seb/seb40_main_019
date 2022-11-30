270ad7c348ed8315da65de89519ea41def9ff0b3
shinker1002 <shinker1002@naver.com>
2022-11-30 13:48:05 +0900

    Fix: 장바구니, 상품 주문, 카트 담기 유저 확인 추가

--- client/src/pages/shopProductOrder/js/OrderMobileButton.js
+++ client/src/pages/shopProductOrder/js/OrderMobileButton.js
@@ -1,7 +1,22 @@
 import '../css/orderMobileButton.scss';
+import { useSelector } from 'react-redux';
 
-export default function OrderMobileButton({ totalPrice, setModal }) {
+export default function OrderMobileButton({ totalPrice, myPoint, setModal }) {
+  const user = useSelector((state) => state.user);
+
   const payment = () => {
+    if (user.userRole === '') {
+      window.alert('로그인이 필요한 서비스입니다');
+      return;
+    }
+    if (user.userRole === 'ROLE_ADMIN_TEST' || user.userRole === 'ROLE_ADMIN') {
+      window.alert('판매자는 이용할 수 없습니다.');
+      return;
+    }
+    if (myPoint < totalPrice + 3000) {
+      window.alert('포인트가 부족합니다');
+      return;
+    }
     setModal(true);
   };
 
