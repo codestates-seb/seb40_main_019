e50a6b30f4cfa13b6a4c10687c998797f1dbcddd
shinker1002 <shinker1002@naver.com>
2022-12-01 18:30:23 +0900

    Fix: 결제 리다이렉트 url 수정

--- client/src/util/api/payment.js
+++ client/src/util/api/payment.js
@@ -35,8 +35,8 @@ export const tossPay = (nickname, amount) => {
         orderId: `${randomId}`, // 상점에서 주문 건을 구분하기 위해 발급한 고유 ID입니다. 문자열이어야 합니다.
         orderName: `포인트결제`, // 결제에 대한 주문명입니다. 예를 들면 생수 외 1건 같은 형식입니다. 최대 길이는 100자입니다.
         customerName: `${nickname}`, // 고객의 이름입니다. 최소 1글자 이상 최대 10글자 이하여야 합니다.
-        successUrl: 'http://localhost:3000/payment/success', // 성공시 리다이렉트 주소
-        failUrl: 'http://localhost:3000/payment/failed', // 실패시 리다이렉트 주소
+        successUrl: `/payment/success`, // 성공시 리다이렉트 주소
+        failUrl: `/payment/failed`, // 실패시 리다이렉트 주소
         flowMode: 'DIRECT',
         easyPay: '토스페이',
       });
