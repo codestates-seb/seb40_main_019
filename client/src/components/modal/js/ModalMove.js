53a213ef9bbde26d095bbfb50a8c291d64529e44
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-12-01 14:36:35 +0900

    Feat: navbar 알람창

--- client/src/components/modal/js/ModalMove.js
+++ client/src/components/modal/js/ModalMove.js
@@ -1,5 +1,60 @@
 import '../css/Modal.scss';
+import { Transition } from 'react-transition-group';
+import { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function ModalMove() {
-  return <></>;
+export default function ModalMove({ setModalOn, modalOn, modalText, link }) {
+  const navigate = useNavigate();
+  const box = useRef();
+  const handleColseModal = (e) => {
+    if (modalOn && (!box.current || !box.current.contains(e.target)))
+      setModalOn(false);
+  };
+  useEffect(() => {
+    if (modalOn) {
+      window.addEventListener('click', handleColseModal);
+      return () => {
+        window.removeEventListener('click', handleColseModal);
+      };
+    }
+  }, []);
+  return (
+    <Transition
+      in={modalOn}
+      timeout={{ enter: 0, exit: 200 }}
+      appear
+      unmountOnExit
+    >
+      <div
+        className={
+          modalOn ? 'modalBackground modalOpen' : 'modalBackground modalClose'
+        }
+        ref={box}
+      >
+        <div className="modalBox">
+          <div className="modalText">
+            <h3>{modalText}</h3>
+          </div>
+          <div className="modalYesorNoBtn">
+            <button
+              className="close"
+              onClick={() => {
+                setModalOn(false);
+              }}
+            >
+              취소
+            </button>
+            <button
+              onClick={() => {
+                setModalOn(false);
+                navigate(link);
+              }}
+            >
+              이동
+            </button>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  );
 }
