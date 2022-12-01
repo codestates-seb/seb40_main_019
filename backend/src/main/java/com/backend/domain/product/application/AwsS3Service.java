56dcbe8cb4f0b0c264b60271a8cc0a0ea03d6776
hyojoonm <ssjj008z@naver.com>
2022-12-01 20:13:59 +0900

    feat: img 리사이징 완료

--- backend/src/main/java/com/backend/domain/product/application/AwsS3Service.java
+++ backend/src/main/java/com/backend/domain/product/application/AwsS3Service.java
@@ -41,7 +41,7 @@ public class AwsS3Service implements ImageUploadService{
         String fileName = createFileName(img.getOriginalFilename());
         String fileFormatName = img.getContentType().substring(img.getContentType().lastIndexOf("/") + 1);
 
-        MultipartFile resizedFile = resizeImage(fileName, fileFormatName, img, 768);
+        MultipartFile resizedFile = resizeImage(fileName, fileFormatName, img, 500);
 
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentType(img.getContentType());
@@ -78,19 +78,19 @@ public class AwsS3Service implements ImageUploadService{
             int originHeight = image.getHeight();
 
             // origin 이미지가 resizing될 사이즈보다 작을 경우 resizing 작업 안 함
-            if(originWidth < targetWidth)
-                return originalImage;
 
             MarvinImage imageMarvin = new MarvinImage(image);
 
             Scale scale = new Scale();
             scale.load();
             scale.setAttribute("newWidth", targetWidth);
-            scale.setAttribute("newHeight", targetWidth * originHeight / originWidth);
+            scale.setAttribute("newHeight", targetWidth );
             scale.process(imageMarvin.clone(), imageMarvin, null, null, false);
 
             BufferedImage imageNoAlpha = imageMarvin.getBufferedImageNoAlpha();
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(imageNoAlpha, fileFormatName, baos);
+            baos.flush();
 
             return new MockMultipartFile(fileName, baos.toByteArray());
 
@@ -113,14 +113,4 @@ public class AwsS3Service implements ImageUploadService{
         }
     }
 
-    private static String createStoreFileName(String originalFilename) {
-        String ext = extractExt(originalFilename);
-        String uuid = UUID.randomUUID().toString();
-        return uuid + "." + ext;
-    }
-
-    private static String extractExt(String originalFilename) {
-        int pos = originalFilename.lastIndexOf(".");
-        return originalFilename.substring(pos + 1);
-    }
 }
