9e6f858f2d9719f2de8895cbb834af572f55c7c7
JY <jiyeon2954@gmail.com>
2022-12-02 20:21:10 +0900

    포인트 결제

--- client/src/components/mypage/mypagePoint/js/MypagePointButton.js
+++ client/src/components/mypage/mypagePoint/js/MypagePointButton.js
@@ -22,42 +22,55 @@ export default function MypagePointButton() {
   return (
     <>
       <div className="pointBtnArea">
-        <div className="pointBtnFlex">
-          <div className="pointBtn">
-            {/* <p>포인트</p> */}
-            <div className="pointCircle colorBlue">
-              <FontAwesomeIcon className="pointIcon" icon={faCoins} />
-            </div>
+        <div className="titleBox">
+          <div className="pointMargin">
+            <p>포인트</p>
           </div>
-          <div className="point">
-            <p>{formatMoney(point)}</p>
+          <div className="pointBtnFlex">
+            <div className="pointBtn">
+              {/* <p>포인트</p> */}
+              <div className="pointCircle colorBlue">
+                <FontAwesomeIcon className="pointIcon" icon={faCoins} />
+              </div>
+            </div>
+            <div className="point">
+              <p>
+                <br />
+                {formatMoney(point)}
+              </p>
+            </div>
           </div>
         </div>
-        <div className="pointBtnFlex">
-          <div className="pointBtn">
-            {/* <p>포인트 충전</p> */}
-            <div
-              // onClick={() => tossPay(user.nickname, value)}
-              className="pointCircle colorYellow"
-            >
-              <FontAwesomeIcon className="pointIcon" icon={faCoins} />
+
+        <div className="titleBox">
+          <div className="chargeMargin">
+            <p>포인트 충전</p>
+          </div>
+          <div className="pointBtnFlex">
+            <div className="pointBtn">
+              <div className="pointCircle colorYellow">
+                <FontAwesomeIcon className="pointIcon" icon={faCoins} />
+              </div>
+            </div>
+            <div className="point">
+              <input
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                type="text"
+                placeholder="포인트 입력"
+              />
+              <div className="chargeContainer">
+                <button
+                  onClick={() => tossPay(user.nickname, value)}
+                  className="yellowBtn"
+                >
+                  충전
+                </button>
+              </div>
             </div>
           </div>
-          <div className="point">
-            <input
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-              type="text"
-              placeholder="포인트 입력"
-            />
-            <div className="chargeContainer">
-              <button
-                onClick={() => tossPay(user.nickname, value)}
-                className="yellowBtn"
-              >
-                충전
-              </button>
-            </div>
+          <div className="memo">
+            <p>실제 결제되지 않습니다.</p>
           </div>
         </div>
       </div>
