89cbb7039948c613161f3e8006fb965cfcd43be6
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-12-02 18:16:12 +0900

    Fix: 상세페이지 사이드바수정

--- client/src/components/layout/js/ShopLayout.js
+++ client/src/components/layout/js/ShopLayout.js
@@ -1,52 +1,24 @@
 import '../css/shopLayout.scss';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Sidebar from '../../sidebar/js/Sidebar';
 
 export default function ShopLayout() {
+  const { pathname } = useLocation();
+  console.log(pathname);
   return (
     <>
       <div className="mainInner">
-        {/* <div className="sidebar1280">
-          <div className="sidebarAb">
-            <Sidebar />
+        {location.pathname.split('/')[2] === 'detail' ? null : (
+          <div className="sideBar">
+            <div className="sticky">
+              <Sidebar />
+            </div>
           </div>
-        </div> */}
-        <div className="sideBar">
-          <div className="sticky">
-            <Sidebar />
-          </div>
-        </div>
+        )}
         <div className="container">
           <Outlet />
-          {/* <div className="test"></div> */}
         </div>
       </div>
     </>
   );
 }
-// import '../css/TotalLayout.scss';
-// import { Outlet } from 'react-router-dom';
-// import Sidebar from '../../sidebar/js/Sidebar';
-
-// export default function ShopLayout() {
-//   return (
-//     <>
-//       <div className="sidebar1280">
-//         <div className="sidebarAb">
-//           <Sidebar />
-//         </div>
-//       </div>
-//       <div className="totalLayout">
-//         <div className="sidebarLayout">
-//           <div className="sidebar">
-//             <Sidebar />
-//           </div>
-//         </div>
-//         <div className="contentLayout">
-//           <Outlet />
-//         </div>
-//         <div className="marginLayout"></div>
-//       </div>
-//     </>
-//   );
-// }
