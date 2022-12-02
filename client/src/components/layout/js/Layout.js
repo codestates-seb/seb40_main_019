d9ec7243c53c78fcadb0cc3871bef12093e70584
Liieonking <sji12251225@gmail.com>
2022-12-02 15:43:40 +0900

    Fix: 판매자 푸터 노란색

--- client/src/components/layout/js/Layout.js
+++ client/src/components/layout/js/Layout.js
@@ -1,11 +1,14 @@
 import '../css/layout.scss';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../../header/js/Header';
 import Main from '../../main/js/Main';
 import Footer from '../../footer/js/Footer';
+import FooterYellow from '../../footer/js/FooterYellow';
 import Nav from '../../header/js/Nav';
 
 export default function Layout() {
+  const location = useLocation();
+
   return (
     <>
       <div className="appLayout">
@@ -15,7 +18,11 @@ export default function Layout() {
         <Main>
           <Outlet />
         </Main>
-        <Footer />
+        {location.pathname.split('/')[1] === 'seller' ? (
+          <FooterYellow />
+        ) : (
+          <Footer />
+        )}
       </div>
     </>
   );
