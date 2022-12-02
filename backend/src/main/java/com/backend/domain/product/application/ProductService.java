53b7afae83b4ac79d837d96b732e4c7c3391c066
taekgil99 <pjwlover2005@naver.com>
2022-12-03 02:05:42 +0900

    Refactor: checkAccess() update

--- backend/src/main/java/com/backend/domain/product/application/ProductService.java
+++ backend/src/main/java/com/backend/domain/product/application/ProductService.java
@@ -38,18 +38,19 @@ public class ProductService {
     private final ReviewRepository reviewRepository;
     private final CategoryRepository categoryRepository;
     private final AwsS3Service awsS3Service;
+
     // 제품 생성
     @SneakyThrows
     @Transactional
-    public Product create(Long userId, int price, String productName,TitleImg titleImg,DetailImg detailImg,String tag,Long categoryId){
+    public Product create(Long userId, int price, String productName, TitleImg titleImg, DetailImg detailImg, String tag, Long categoryId) {
 
 
         log.info(" 프로젝트 새로 생성 ");
 
         User user = userRepository.findById(userId).orElseThrow(MemberNotFound::new);
-        log.info(" user : " ,user);
+        log.info(" user : ", user);
         Category category = categoryRepository.findById(categoryId).orElseThrow(CategoryNotFound::new);
-        log.info("category : ",category);
+        log.info("category : ", category);
 
         String titleUrl = awsS3Service.StoreImage(titleImg.getTitleImg());
         log.info("타이틀 이미지 완료");
@@ -75,17 +76,17 @@ public class ProductService {
     // 상품 수정
     @SneakyThrows
     @Transactional
-    public Product update(Long productId, Long categoryId, int price , String productName, TitleImg titleImg, DetailImg detailImg, User user) {
+    public Product update(Long productId, Long categoryId, int price, String productName, TitleImg titleImg, DetailImg detailImg, User user) {
         Product findProduct = productRepository.findById(productId).orElseThrow(ProductNotFound::new);
 
         checkAccess(productId, user, findProduct);
 
-        log.info(" findProduct : ",findProduct);
+        log.info(" findProduct : ", findProduct);
         Category category = categoryRepository.findById(categoryId).orElseThrow(CategoryNotFound::new);
         log.info(" category : ", category);
         log.info(" 수정 카테고리 입력 ");
         Optional.ofNullable(category)
-                        .ifPresent(findProduct::setCategory);
+                .ifPresent(findProduct::setCategory);
         log.info(" 수정 상품 아이디 입력 ");
         Optional.ofNullable(productId)
                 .ifPresent(findProduct::setProductId);
@@ -98,12 +99,12 @@ public class ProductService {
         log.info(" 수정 타이틀 이미지 입력 ");
 
 
-        if ( titleImg.getTitleImg() != null) {
+        if (titleImg.getTitleImg() != null) {
             awsS3Service.deleteImage(findProduct.getTitleImg());
             findProduct.setTitleImg(awsS3Service.StoreImage(titleImg.getTitleImg()));
         }
         log.info(" 타이틀 이미지 완료 ");
-        if ( detailImg.getDetailImg() != null) {
+        if (detailImg.getDetailImg() != null) {
             awsS3Service.deleteImage(findProduct.getDetailImg());
             findProduct.setDetailImg(awsS3Service.DetailImage(detailImg.getDetailImg()));
         }
@@ -113,10 +114,12 @@ public class ProductService {
     }
 
     private static void checkAccess(Long productId, User user, Product findProduct) {
-        if ((!user.getEmail().equals("admin@luxmeal.xyz") && !user.getUserRole().equals("ROLE_ADMIN"))
-        ||(!Objects.equals(findProduct.getUser().getUserId(), user.getUserId()) || productId <= 37)) {
-            throw new BusinessLogicException(ExceptionCode.HANDLE_ACCESS_DENIED);
+        if (user.getEmail().equals("admin@luxmeal.xyz") && user.getUserRole().equals("ROLE_ADMIN")) {
+            return;
+        } else if (productId <= 37) {
+            return;
         }
+        throw new BusinessLogicException(ExceptionCode.HANDLE_ACCESS_DENIED);
     }
 
     @Transactional
@@ -128,19 +131,20 @@ public class ProductService {
         log.info(" product : ", product);
         productRepository.delete(product);
         log.info(" 상품 삭제 ");
-        return  productId;
+        return productId;
     }
 
     @Transactional
-    public Page<Product> getLists(int page,int size,int filter ) {
+    public Page<Product> getLists(int page, int size, int filter) {
         log.info("전체 리스트 조회");
-        String str ="productId";
-        if(filter== 1) { str = "productId";}
-        else if (filter== 2) { str = "viewCount";}
-        else if (filter == 3 ) {
+        String str = "productId";
+        if (filter == 1) {
+            str = "productId";
+        } else if (filter == 2) {
+            str = "viewCount";
+        } else if (filter == 3) {
             str = "price";
-        }
-        else if (filter == 4 ) {
+        } else if (filter == 4) {
             return productRepository.findAll(PageRequest.of(page, size, Sort.by("price").ascending()));
         }
         return productRepository.findAll(PageRequest.of(page, size, Sort.by(str).descending()));
@@ -148,7 +152,7 @@ public class ProductService {
 
     // 제품 이름이 같을 경우 오류 발생
     private void existSameName(String productName) {
-        if (productRepository.existsByProductName(productName)){
+        if (productRepository.existsByProductName(productName)) {
             // 상품 이름이 겹치면 에러
             log.info(" 상품 이름이 겹침");
             throw new ProductExist();
@@ -162,14 +166,15 @@ public class ProductService {
         product.ViewCountPlus(product.getViewCount());
         return product;
     }
+
     @Transactional
-    public String calculateReviewAverage(Long productId){
+    public String calculateReviewAverage(Long productId) {
         log.info("평점 계산 상품 검색 ");
         List<Review> reviews = reviewRepository.findByProductId(productId);
-        if (reviews.isEmpty()){
+        if (reviews.isEmpty()) {
             log.info("리뷰가 없어요");
             return "0";
-        }else {
+        } else {
             log.info("리뷰 있다");
             int totalScore = reviews.stream()
                     .mapToInt(Review::getStar)
@@ -179,22 +184,25 @@ public class ProductService {
             double result = ((double) totalScore / (double) totalUsers);
 
             DecimalFormat form = new DecimalFormat("#.##");
-            log.info("result : ",result);
+            log.info("result : ", result);
             return form.format(result);
         }
     }
+
     @Transactional
-    public Page<Product> getCategory(Long categoryId,int filter,int page, int size) {
-        log.info(" 카테고리 별 상품 조회 ");String str ="productId";
-        if(filter== 1) { str = "productId";}
-        else if (filter== 2) { str = "viewCount";}
-        else if (filter == 3 ) {
+    public Page<Product> getCategory(Long categoryId, int filter, int page, int size) {
+        log.info(" 카테고리 별 상품 조회 ");
+        String str = "productId";
+        if (filter == 1) {
+            str = "productId";
+        } else if (filter == 2) {
+            str = "viewCount";
+        } else if (filter == 3) {
             str = "price";
+        } else if (filter == 4) {
+            return productRepository.findByCategory(categoryId, PageRequest.of(page, size, Sort.by("price").ascending()));
         }
-        else if (filter == 4 ) {
-            return productRepository.findByCategory(categoryId,PageRequest.of(page, size, Sort.by("price").ascending()));
-        }
-        return productRepository.findByCategory(categoryId,PageRequest.of(page,size,Sort.by(str).descending()));
+        return productRepository.findByCategory(categoryId, PageRequest.of(page, size, Sort.by(str).descending()));
     }
 
     @Transactional
@@ -205,13 +213,13 @@ public class ProductService {
 
         int[] arr = new int[3];
 
-        for (int i =0; i<arr.length;i++){
-            arr[i] = rnd.nextInt(10) +1; // 1부터 10까지 랜덤 숫자 뽑음
-            for (int j = 0; j<i;j++){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rnd.nextInt(10) + 1; // 1부터 10까지 랜덤 숫자 뽑음
+            for (int j = 0; j < i; j++) {
                 if (arr[i] == arr[j]) i--;  // 중복제거
             }
         }
-        for (int i : arr){
+        for (int i : arr) {
             Product product = productRepository.findById((long) i).orElseThrow(ProductNotFound::new);
             list.add(product);
         }
