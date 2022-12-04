591f5bdfc213091314be445ffbe0b44cd9f42eee
taekgil99 <pjwlover2005@naver.com>
2022-12-05 07:52:50 +0900

    Refactor: 코드 정리

--- backend/src/main/java/com/backend/domain/review/api/ReviewController.java
+++ backend/src/main/java/com/backend/domain/review/api/ReviewController.java
@@ -104,7 +104,6 @@ public class ReviewController {
     @GetMapping("/review/seller")
     public ResponseEntity getProductReview(@RequestParam int page){
         int size = 15;
-//        Long userId = authUser.getUser().getUserId();
         Page<Review> reviews = reviewService.getProductReview(page-1, size);
         List<Review> content = reviews.getContent();
         return new ResponseEntity(new MultiResponse<>(reviewMapper.reviewsToReviewResponseDto(content),reviews),HttpStatus.OK);
