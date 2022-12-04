742782788e0d18f9f1bdc9ae2495196cd378b0b3
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-12-04 20:45:10 +0900

    fix: 리뷰 상세페이지이동

--- client/src/components/seller/js/Review.js
+++ client/src/components/seller/js/Review.js
@@ -1,14 +1,21 @@
+import { Link } from 'react-router-dom';
+import { formatDate } from '../../../util/function/formatData';
 import ReviewStar from '../../review/js/ReviewStar';
 import '../css/Review.scss';
 
 export default function Review({ item }) {
+  console.log(item);
   return (
     <div className="sellerproduct review">
       <div className="createdAt">
-        <p>{item.createdAt}</p>
+        <p>{formatDate(item.createdAt)}</p>
       </div>
-      <img className="titleImg" src={item.titleImg} alt="productImg" />
-      <div className="title">{item.productName}</div>
+      <Link to={`/product/detail/${item.proId}`}>
+        <img className="titleImg" src={item.titleImg} alt="productImg" />
+      </Link>
+      <Link to={`/product/detail/${item.proId}`}>
+        <div className="title">{item.productName}</div>
+      </Link>
       <div className="reviewcontent">{item.reviewContent}</div>
       <div className="star">
         <ReviewStar clickStar={item.star} type={'small'} />
